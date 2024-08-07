aa6624aa5b3ae77df567f060c3587be0a26f3463
Ryan Bowman <[email protected]>
2024-08-07 13:18:29 -0400

    In the html file I created a <p> element under the 'select' button to display a message whenever the user performs an action with the button. After this, I created the 'changeSize()' function to handle the prompt the user answers and set their answer to the new size of the sketch container. I placed this changeSize() function in the top domContentLoaded function. Finally, whenever the above process was completed by the user, it would not reset the board, but would add more boxes at the bottom of the sketch container, so I needed to set the sketchBoard's initial innerHTML to ' ' whenever that function was called.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     createSketchBoard(16);
+
+    const selectSizeButton = document.getElementById('select-size');
+    selectSizeButton.addEventListener('click', function () {
+        let size = changeSize();
+        createSketchBoard(size);
+    })
 })
 
 function createSketchBoard(size) {
     const sketchBoard = document.getElementById('sketch-container');
-
+    sketchBoard.innerHTML = '';
+    
     sketchBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     sketchBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
@@ -14,5 +21,18 @@ function createSketchBoard(size) {
         let newSquare = document.createElement('div');
         newSquare.style.border = "1px solid black";
         sketchBoard.appendChild(newSquare)
+    }
+}
+
+function changeSize(){
+    let newSize = prompt('Please select the size of your board. Please write your answer in an integer between 1-100.');
+    let submitFeedback = document.getElementById('submit-feedback')
+    if (newSize == ''){
+        submitFeedback.innerHTML = "Please try again by typing in an integer between 1 and 100."
+    } else if (newSize < 1 || newSize > 100) {
+        submitFeedback.innerHTML = "Please make sure to submit an integer between 1 and 100."
+    } else {
+        submitFeedback.innerHTML = "Let's begin!"
+        return newSize;
     }
 }
